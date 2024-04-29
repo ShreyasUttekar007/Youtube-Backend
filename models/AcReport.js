55552af9e87d383c86b8b5df086bbd8c47037da2
ShreyasUttekar007 <shreyas.uttekar@showtimeconsulting.in>
2024-04-29 18:32:02 +0530

    first commit

---
+++ models/AcReport.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const AcReportSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: String,
+    },
+    thumbnail: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    url: {
+      type: String,
+    },
+    source: {
+      type: String,
+    },
+    views: {
+      type: String,
+    },
+    likes: {
+      type: String,
+    },
+    comments: {
+      type: String,
+    },
+    duration: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+AcReportSchema.pre("save", async function () {
+  try {
+    await this.populate("userId", "email").execPopulate();
+    console.log("User Email:", this.userId.email);
+  } catch (error) {
+    console.error("Error during population:", error);
+  }
+});
+
+const Report = mongoose.model("Report", AcReportSchema);
+
+module.exports = Report;
---
+++ routes/report.js
@@ -0,0 +1,206 @@
+const express = require("express");
+const router = express.Router();
+const Report = require("../models/AcReport");
+const { roles } = require("../models/User");
+const authenticateUser = require("../middleware/authenticateUser");
+
+router.use(authenticateUser);
+
+router.post("/report", async (req, res) => {
+  try {
+    const momData = req.body;
+    if (momData.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ error: "Forbidden - Unauthorized user" });
+    }
+    const newMom = await Report.create(momData);
+    res.status(201).json(newMom);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/get-report", async (req, res) => {
+  try {
+    let moms;
+    if (req.query.perPage === "all") {
+      moms = await Report.find().populate("userId");
+    } else {
+      const perPage = parseInt(req.query.perPage);
+      moms = await Report.find()
+        .populate("userId")
+        .limit(perPage);
+    }
+    res.status(200).json(moms);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.get("/get-top-10-reports", async (req, res) => {
+  try {
+    const topReports = await Report.find().sort({ views: -1 }).limit(10).populate("userId");
+    res.status(200).json(topReports);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+router.get("/get-report-by-id/:momId", async (req, res) => {
+  try {
+    const { momId } = req.params;
+    console.log("momId::: ", momId);
+    const mom = await Report.findById(momId).populate("userId");
+
+    if (!mom) {
+      return res.status(404).json({ error: "MOM not found" });
+    }
+
+    res.status(200).json(mom);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// router.get("/get-report/:userId", async (req, res) => {
+//   try {
+//     const { userId } = req.params;
+//     const userRoles = req.user?.roles || [];  
+
+//     if (userRoles.includes("admin")) {
+//       const moms = await Report.find().populate("userId");
+//       return res.status(200).json(moms);
+//     }
+
+//     if (userId !== req.user._id.toString()) {
+//       return res.status(403).json({ error: "Forbidden - Unauthorized user" });
+//     }
+
+//     let moms;
+//     const zoneRoles = [
+//       "Eastern Vidarbha",
+//       "Konkan",
+//       "Marathwada",
+//       "Mumbai",
+//       "Northern Maharashtra",
+//       "Thane + Palghar",
+//       "Western Maharashtra",
+//       "Western Vidarbha",
+//     ];
+
+//     const userZoneRoles = (userRoles || []).filter(role => zoneRoles.includes(role));
+
+//     if (userZoneRoles.length > 0) {
+//       moms = await Report.find({ zone: { $in: userZoneRoles } }).populate("userId");
+//     } else {
+//       moms = await Report.find({ userId }).populate("userId");
+//     }
+
+//     return res.status(200).json(moms);
+//   } catch (error) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// });
+
+
+// router.get("/get-report-by-party/:partyName", async (req, res) => {
+//   try {
+//     const { partyName } = req.params;
+//     const moms = await Report.find({ partyName }).populate("userId");
+
+//     const momCount = await Report.countDocuments({ partyName });
+
+//     res.status(200).json({ moms, momCount });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// router.get("/get-report-by-constituency/:constituency", async (req, res) => {
+//   try {
+//     const { constituency } = req.params;
+
+//     const moms = await Report.find({ constituency }).populate("userId");
+
+//     const momCount = await Report.countDocuments({ constituency });
+
+//     res.status(200).json({ moms, momCount });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+// router.get("/get-report-by-zone/:zone", async (req, res) => {
+//   try {
+//     const { zone } = req.params;
+
+//     const moms = await Report.find({ zone });
+
+//     const momCount = await Report.countDocuments({ zone });
+
+//     res.status(200).json({ moms, momCount });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// router.get("/get-report-by-pc/:pc", async (req, res) => {
+//   try {
+//     const { pc } = req.params;
+//     const moms = await Report.find({ pc }).populate("userId");
+
+//     const momCount = moms.length;
+
+//     res.status(200).json({ moms, momCount });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+// router.get("/get-all-moms-count", async (req, res) => {
+//   try {
+//     const { pc, constituency } = req.query;
+//     let moms;
+
+//     if (pc) {
+//       moms = await Report.find({ pc }).populate("userId");
+//     } else if (constituency) {
+//       moms = await Report.find({ constituency }).populate("userId");
+//     } else {
+//       moms = await Report.find({});
+//     }
+//     const momCount = moms.length;
+//     res.status(200).json({ moms, momCount });
+//   } catch (error) {
+//     res.status(500).json({ error: error.message });
+//   }
+// });
+
+router.put("/update-report/:momId", async (req, res) => {
+  try {
+    const { momId } = req.params;
+    const updatedMom = await Report.findByIdAndUpdate(momId, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedMom);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/delete-report/:momId", async (req, res) => {
+  try {
+    const { momId } = req.params;
+    const deletedMom = await Report.findByIdAndDelete(momId);
+    if (!deletedMom) {
+      return res.status(404).json({ error: "Report record not found" });
+    }
+    res.status(200).json({ message: "Report record deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+module.exports = router;
